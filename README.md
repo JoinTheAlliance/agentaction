eeccb2b485ae5e7330852a00afbe8406686f4d9b
moon <shawmakesmagic@gmail.com>
2023-07-15 17:39:50 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# actionflow
+
+Action chaining and history for agents
+
+<img src="resources/image.jpg">
+
+# Quickstart
+
+# TODO
+
+# Installation
+
+```bash
+pip install actionflow
+```
+
+# Usage
+
+# TODO
+
+# Testing
+```
+pytest test.py
+```
+
+# Publishing
+
+```bash
+bash publish.sh --version=<version> --username=<pypi_username> --password=<pypi_password>
+```
+
+# Contributions Welcome
+
+If you like this library and want to contribute in any way, please feel free to submit a PR and I will review it. Please note that the goal here is simplicity and accesibility, using common language and few dependencies.
+
+<img src="resources/youcreatethefuture.jpg">
---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+readme = ""
+with open("README.md", "r") as fh:
+    readme = fh.read()
+    # search for any lines that contain <img and remove them
+    readme = readme.split("\n")
+    readme = [line for line in readme if "<img" not in line]
+    # now join all the lines back together
+    readme = "\n".join(readme)
+
+
+setup(
+    name="actionflow",
+    version="0.1.0",
+    description="Action chaining and history for agents",
+    long_description=readme,  # added this line
+    long_description_content_type="text/markdown",  # and this line
+    url="https://github.com/AutonomousResearchGroup/actionflow",
+    author="Moon",
+    author_email="shawmakesmagic@gmail.com",
+    license="MIT",
+    packages=["actionflow"],
+    install_requires=["pynput"],
+    readme="README.md",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
